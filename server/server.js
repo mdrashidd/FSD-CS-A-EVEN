4b2e57b0ffbc0e9330db3d5d6ac3d3d8bf965249
Mohammad Rashid <ashrafii.rashid@gmail.com>
2026-02-13 13:06:40 +0530

    Server Initial

--- server/server.js
+++ server/server.js
@@ -1,16 +1,45 @@
+import { error } from 'console';
 import http from 'http';
+import os from "os";
+
+let PORT=4001;
+let body=[];
 const sever=http.createServer((req,res)=>{
 const url=req.url;
-if(url=="/"){
-    res.write("Home Page");
-}else if(url=="/about"){
-res.write("About Page");
-}else if(url=="/contact"){
-    res.write("Contact Page");
-}else{
-    res.write("Error Page");
+if(url=="/" && req.method=="GET"){
+    res.end("Home Page");
+}else if(url=="/about" && req.method=="GET"){
+res.write("<h1>About Page<h1>");
+}else if(url=="/contact" && req.method=="GET"){
+    res.end("<h1>Contact Page<h1>");
+}else if(url=="/system" && req.method=="GET"){
+    res.end("<h1>Server Info<h1>");
+}else if(url=="/senddata" && req.method=="POST"){
+
+    
+    req.on("data",(chunk)=>{
+        body.push(chunk);
+    })
+    
+    req.on("end",()=>{
+        req.statusCode=201;
+    console.log(body, "Data send");
+         res.end(body+ " <h1>Status Code: 201 <h1>");
+    })
+    
+   
+}else if(url=="/viewdata" && req.method=="GET"){
+    res.write("View Page");
+    res.end(body);
+
 }
-res.end();
+else{
+    req.statusCode=404;
+    res.end("<h1>Error Page<h1>");
+}
+
 })
 
-sever.listen(4001);
+sever.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+});
